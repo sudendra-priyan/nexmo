9c32385537abfc705868ff55b3ef730e54e984b4
Sudendra Priyan <sudendra.94@gmail.com>
2021-07-17 23:35:28 +0530

    Readme update and pushing additional success logs

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
-# nexmo
+# Nexmo API Automation
+
+### Tech
+
+* Python - Programming Language used for writing test.
+* PyTest - Testing Framework used.
+
+
+### Installation
+
+The Framework requires [Python 3.7](https://www.python.org/downloads/release/python-377/) and [PIP](https://www.poftut.com/how-to-install-pip-on-macos/) to run.
+
+
+Prerequisite for running a test case:
+```sh
+$ cd nexmo 
+$ sudo pip3 install pipenv
+$ pipenv install 
+$ pipenv shell
+```
+
+Executing the above commands will create a pip environment:
+```sh
+(nexmo) sudendrapriyan@192 nexmo
+```
+JWT Token
+
+```sh
+open config.yaml file and update your JWT Token
+```
+
+Sequential Test Execution example:
+
+```sh
+pytest -s -vv --env dev
+```
+
+Parallel Test Execution example:
+```sh
+pytest -s -vv  --workers 1 --tests-per-worker 10  --env dev
+
+The above command will execute the test in 10 threads
+```
+
+
+Findig the test logs
+
+```sh
+* Once the test execution is done, Check for the logs under test_logs folder
+* The logs will be created under a sub folder with the format - logs-17-29-19
+* 17 - Date, 29 - Hour , 19 - Second, This will allow us identify each test runs.
+```
--- test_cases/test_conversations_api.py
+++ test_cases/test_conversations_api.py
@@ -1,4 +1,5 @@
 import pytest
+from utilities.temp_logger import log
 from base.conversation_api import Conversation
 from utilities.generic_method import GenericMethod
 
@@ -23,6 +24,7 @@ class TestConversation:
         response = self.conversation.create_conversation(input_data=self.creation_input)
         assert response, f"Expected a valid response but got -{response}"
         assert response.status_code == 200, f"Expected status is 200 but got -{response.status_code}"
+        log("====Successfully executed conversation creation====")
 
     def test_retrieve_conversation(self):
         conversation_id = self.conversation.create_conversation(input_data=self.creation_input, parse=True)
@@ -30,6 +32,7 @@ class TestConversation:
         retrieved_id = self.conversation.retrieve_conversation(conversation_id=conversation_id, parse=True)
         assert retrieved_id, f"Conversation retrieval Failed"
         assert retrieved_id == conversation_id, "Failed due to Conversation ID mismatch"
+        log("====Successfully verified retrieve conversation====")
 
     def test_list_conversation(self):
         conversation_id = self.conversation.create_conversation(input_data=self.creation_input, parse=True)
@@ -38,6 +41,7 @@ class TestConversation:
         assert list_of_conversation, "Failed to get the list of conversation"
         assert len(
             list_of_conversation) != 0, f"List conversation api hit was successful but no data was returned -{list_of_conversation}"
+        log("====Successfully verified list conversation====")
 
     def test_update_conversation(self):
         conversation_id = self.conversation.create_conversation(input_data=self.creation_input, parse=True)
@@ -50,6 +54,7 @@ class TestConversation:
         assert response.status_code == 200, f"Expected status is 200 for update conversation but got -{response.status_code}"
         assert self.conversation.parse_response(response)[
                    'id'] == conversation_id, "Conversation ID changed after updating"
+        log("====Successfully verified update conversation====")
 
     def test_delete_conversation(self):
         conversation_id = self.conversation.create_conversation(input_data=self.creation_input, parse=True)
@@ -60,6 +65,7 @@ class TestConversation:
 
         retrieved_id = self.conversation.retrieve_conversation(conversation_id=conversation_id, parse=False)
         assert retrieved_id.status_code != 200 and retrieved_id.status_code == 404, f"Conversation was deleted before, but it is successfully fetched"
+        log("====Successfully verified delete conversation====")
 
     def test_record_conversation(self):
         conversation_id = self.conversation.create_conversation(input_data=self.creation_input, parse=True)
@@ -67,3 +73,4 @@ class TestConversation:
 
         record_status = self.conversation.record_conversation(conversation_id=conversation_id, parse=True)
         assert record_status, f"Conversation record Failed"
+        log("====Successfully verified record conversation====")
